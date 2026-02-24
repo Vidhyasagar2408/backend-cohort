326eb414c6efd5f47fc8f4ad42acf358389afa1c
vidhyasagar <vs1522250@gmail.com>
2026-02-24 23:06:49 +0530

    Day 12

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const userModel = require("../models/user.models");
+const jsonWebToken = require("jsonwebtoken");
+
+const authRouter = express.Router();
+
+authRouter.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const isUserAlreadyExists = await userModel.findOne({ email });
+
+  if (isUserAlreadyExists) {
+    return res.status(400).json({
+      message: "User already exists with this email address",
+    });
+  }
+
+  const user = await userModel.create({
+    name,
+    email,
+    password,
+  });
+
+  const token = jsonWebToken.sign(
+    { id: user._id, email: user.email },
+    process.env.JWT_SECRETE,
+  );
+
+  res.cookie("jwt_token", token);
+
+  res.status(201).json({
+    message: "user created successfully",
+    user,
+    token,
+  });
+});
+
+module.exports = authRouter;
